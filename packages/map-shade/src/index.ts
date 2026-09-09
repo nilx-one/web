0cf5898ccdba75f1e1f939dcb82f1a4151132753
Sasha Lohozinskyy <0x0sky@proton.me>
2026-09-09 19:21:49 +0300

    feat(map): export shade composition surface

---
+++ packages/map-shade/src/index.ts
@@ -0,0 +1,23 @@
+// © 2026 aiaiaiai · aiaiaiai.org
+// SPDX-License-Identifier: MPL-2.0
+
+export {
+  cellAtLngLat,
+  createTapHandler,
+  formatRecords,
+  type CellTap,
+} from "./pick";
+export {
+  createShadeLayer,
+  type ShadeLayer,
+  type ShadeLayerOptions,
+} from "./shade-layer";
+export {
+  createShadeMapFactory,
+  type ShadeMapFactoryOptions,
+  type ShadeRuntime,
+} from "./map-factory";
+export {
+  createRawJournalPresenter,
+  type RawJournalPresenter,
+} from "./presenter";
